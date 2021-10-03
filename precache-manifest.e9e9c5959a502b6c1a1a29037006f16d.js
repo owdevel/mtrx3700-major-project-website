9bf42c435f86585d755ff6796ac0d16dd8f22a58
Owen Mak <omak7591@uni.sydney.edu.au>
2021-10-03 20:57:08 +1100

    gh-pages update Sun Oct 03 2021 20:57:08 GMT+1100 (Australian Eastern Daylight Time)

---
+++ precache-manifest.e9e9c5959a502b6c1a1a29037006f16d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7eef109f993d22891f",
+    "url": "/css/app.7ff1bdfa.css"
+  },
+  {
+    "revision": "604dbd83f44339c4447f",
+    "url": "/css/chunk-vendors.d4799bbd.css"
+  },
+  {
+    "revision": "1701a026b6b131dff0c3135b129f070a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "474f099b3dcb6f723361",
+    "url": "/js/about.3435efeb.js"
+  },
+  {
+    "revision": "bb7eef109f993d22891f",
+    "url": "/js/app.9e7f820a.js"
+  },
+  {
+    "revision": "604dbd83f44339c4447f",
+    "url": "/js/chunk-vendors.3191cb3b.js"
+  },
+  {
+    "revision": "ad64b18fd4ea1b02a1205eac04470b9e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
